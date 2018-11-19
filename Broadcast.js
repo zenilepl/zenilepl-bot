5843975a04aaf3f8453711c87d88d69d13ca8c41
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-11-19 22:16:20 +0200

    Add files via upload

---
+++ Broadcast.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+  const kboosh = new Discord.Client();
+    const kbooshtoken = "NTE0MTUxMDY3ODQ0NDc2OTUx.DtSdLw.ZHPOTMWOmBVhPrx3eiXbwrN0Q4c"
+      kboosh.on('ready', () => {
+        kboosh.user.setGame(`Zenilepl`,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Im Ready!');
+  
+        });
+
+  kboosh.on('message', message => {
+    if (message.content.split(' ')[0] == '=bc')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   kboosh.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "=")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+kboosh.login(kbooshtoken);
---
+++ start.bat
@@ -0,0 +1,2 @@
+node Broadcast.js
+pause
